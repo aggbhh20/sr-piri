c6f391cc9b5bad350ed5938858dbceaec0fc795e
aggbhh20 <aggbhh4@gmail.com>
2022-05-15 09:22:23 +0300

    Imptovements + turkish support

---
+++ CLICK TO EXECUTE PROGRAM.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Program Files\Python310\python.exe" "record.py"
+echo started
--- srpy.py
+++ srpy.py
@@ -5,6 +5,6 @@ smbm = sr.AudioFile("voice.wav")
 print("got wav file")
 with smbm as source:
     audio = srRec.record(source)
-recdaudio = srRec.recognize_google(audio)
+recdaudio = srRec.recognize_google(audio, language="tr-TR")
 print(recdaudio)
 print("should have reccd audio")
