f62ac187fa518968e82af3773ba39b586c00c1e5
aggbhh20 <aggbhh4@gmail.com>
2022-05-15 13:38:22 +0300

    Update srpy.py, Added writing to text files.

--- srpy.py
+++ srpy.py
@@ -5,6 +5,11 @@ smbm = sr.AudioFile("voice.wav")
 print("got wav file")
 with smbm as source:
     audio = srRec.record(source)
+
 recdaudio = srRec.recognize_google(audio, language="tr-TR")
 print(recdaudio)
+with open('recordedtxt.txt', 'w') as f:
+    f.write(recdaudio)
+
+
 print("should have reccd audio")
